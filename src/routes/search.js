fba4d0e45b510fe1501458561990cb553b75b035
Sindre Dahl Løken <sindre.loken@nav.no>
2020-10-12 15:01:26 +0200

    lagt til cors header

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,7 +1,8 @@
 const fetchPamResult = require('../fetchPamResult');
 const transformPamResult = require('../transformPamResult');
+
 module.exports = async function(req, reply) {
   const pamResult = await fetchPamResult(req.query["q"]);
   const payload = await transformPamResult(pamResult);
-  reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send(payload);
+  reply.code(200).header('Content-Type', 'application/json; charset=utf-8').header('Access-Control-Allow-Origin', '*').send(payload);
 };
